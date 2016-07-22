5d5548d57a21b17e58edceaf903c24748f59569c
unknown <ghorisam@yahoo.com>
2016-07-22 17:24:05 +0500

    irst version

--- todolist/main.js
+++ todolist/main.js
@@ -1,41 +1,68 @@
-var DATA = [];
+var title;
+var user;
+        
 $("#btn").click(function()
 {
-   var text = $("#text").val()
-    DATA.push(text);
-    Render(DATA);
-	save();
-	load();
-});
+	var title = $("#title").val()	
+	var user = $("#user").val()
+topicref.push({
+    title:title,
+    user:user,
+    timestamp: Date.now()
+})
+})
 
-function Render(data)
-{
-	   $('#container').empty();
-	for (var i=0;i<data.length;i++)
-	{		 
-     $('#container').append('<div id = "'+i+'"><input type="checkbox">' + data[i]+'<br></div>')
-	 $('#'+i).click(function()
-	 {
-		var index = $(this).attr("id");
-		//$(this).remove();
-		//var index = data.indexOf(myid);
-        if (index > -1)
-		{
-         data.splice(index, 1);
-        }
-		Render(DATA);
-     });	
-    }
-};
-       function save()
-	   {
-	   localStorage.myData = JSON.stringify(DATA);
-       }
-function load()
-{
-	DATA = JSON.parse(localStorage.myData);
-	Render(DATA);
-}
+var topicref = firebase.database().ref('/msgs');
+topicref.on('child_added',function(snapshot){
+   //$('#container').append('<div><a href = "index2.html">'+snapshot.val().title +'</a> posted by:<br>'+snapshot.val().user+'</div>');
+ $('#tab tbody').append('<tr><td><a href = "index2.html">'+snapshot.val().title +'</a></td><td>'+snapshot.val().user+'</td><td>'+new Date(snapshot.val().timestamp)+'</td></tr>')        
+   
+    $('#btn').click(function(){
+       var individualTopicRef = firebase("/msgs" + snapshot.key() + "/comments");
+           individualTopicRef.on('value',function(snapshot){
+		   snapshot.val()          
+})})
+})
 
-load();
-save();
+
+
+
+
+
+
+
+/*var hash = window.location.hash // "#key=-kwewtew"
+      hash = hash.replace("#key=",""); // "-kwewtew"
+    var ref= Firebase("/msgs"+hash+"/comments");
+    ref.child_added
+
+	$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})*/
+
+
+
+/*var DATA = [];
+var un;
+var text;
+
+	   
+	var ref = firebase.database().ref('/msgs');
+        //ref.set();
+        ref.on('child_added',function(snapshot){
+        $('#container').append("<div>"+snapshot.val()+"</div>");
+        });
+        
+        $('#btn').click(function(){
+            
+   text = $("#text").val()
+    un = $("#un").val()
+    ref.push(un + ": "+text);
+        });*/
+
+       /* <script>
+      var hash = window.location.hash // "#key=-kwewtew"
+      hash = hash.replace("#key=",""); // "-kwewtew"
+    var ref= Firebase("/topics/"+hash+"/comments");
+    ref.child_added
+    </script>*/
---
+++ todolist/main2.js
@@ -0,0 +1,6 @@
+var user;
+$("#btn").click(function()
+{	
+	var user = $("#user2").val()
+ref.push(user);
+})
